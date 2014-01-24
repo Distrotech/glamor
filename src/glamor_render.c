aff2f8d9533f565bf5924a673e0702d38b7effbd
Zhigang Gong <zhigang.gong@gmail.com>
2014-01-24 21:39:13 +0800

    Silent some compilation warnings.
    Signed-off-by: Zhigang Gong <zhigang.gong@gmail.com>

--- src/glamor_render.c
+++ src/glamor_render.c
@@ -865,16 +865,15 @@ combine_pict_format(PictFormatShort * des, const PictFormatShort src,
 		    const PictFormatShort mask, enum shader_in in_ca)
 {
 	PictFormatShort new_vis;
-	int src_type, mask_type, src_bpp, mask_bpp;
+	int src_type, mask_type, src_bpp;
 	int i;
 	if (src == mask) {
 		*des = src;
 		return TRUE;
 	}
 	src_bpp = PICT_FORMAT_BPP(src);
-	mask_bpp = PICT_FORMAT_BPP(mask);
 
-	assert(src_bpp == mask_bpp);
+	assert(src_bpp == PICT_FORMAT_BPP(mask));
 
 	new_vis = PICT_FORMAT_VIS(src) | PICT_FORMAT_VIS(mask);
 
