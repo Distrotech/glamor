f674371926323b34a066b01c4fad8a306bfe5717
Eric Anholt <eric@anholt.net>
2014-02-28 13:02:08 -0500

    glamor: Drop bogus _X_UNLIKELY.
    nbox > 4 is actually quite common for spans handling.
    
    Ported from Eric's xserver glamor tree.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- src/glamor_fill.c
+++ src/glamor_fill.c
@@ -204,7 +204,7 @@ _glamor_solid_boxes(PixmapPtr pixmap, BoxPtr box, int nbox, float *color)
 
 	pixmap_priv_get_dest_scale(pixmap_priv, &xscale, &yscale);
 
-	if (unlikely(nbox*4*2 > ARRAY_SIZE(vertices))) {
+	if (nbox * 4 * 2 > ARRAY_SIZE(vertices)) {
 		int allocated_box;
 
 		if (nbox * 6 > GLAMOR_COMPOSITE_VBO_VERT_CNT) {
